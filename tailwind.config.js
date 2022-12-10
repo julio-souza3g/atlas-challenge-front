d74654303f1209f637d33367843aba7ea89f9a39
Julio Souza <julio3g@pop-os.localdomain>
2022-12-10 00:52:44 -0300

    chore: initial config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/components/**/*.tsx',
+    './src/pages/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}

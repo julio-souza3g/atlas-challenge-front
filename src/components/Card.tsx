5d73a5fac83762c3ab64f041ac90b1b9321d4f18
Julio Souza <julio3g@pop-os.localdomain>
2022-12-10 10:53:38 -0300

    feat(src/components/Card.tsx): add show dinamic props from contact

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,11 @@
 import avatar from "../assets/images/avatar.png";
+import { IContact } from "../pages/FriendsList";
 
-export function Card() {
+interface ICardProps {
+  contact: IContact;
+}
+
+export function Card({ contact }: ICardProps) {
   return (
     <>
       <div className="my-1 px-1 py-5 w-full bg-white border border-gray-200 rounded-lg shadow-md">
@@ -10,20 +15,21 @@ export function Card() {
             src={avatar}
             alt="user"
           />
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            Bonnie Green
+          <h5 className="mb-1 text-xl font-medium text-orange-600">
+            {contact.name}
           </h5>
-          <span className="text-sm text-gray-500 dark:text-gray-400">
-            Visual Designer
+          <span className="text-sm text-gray-500">
+            {contact.email}
           </span>
-          <div className="flex mt-4 space-x-3 md:mt-6">
-            <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-orange-600 rounded-lg hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-600">
-              Add friend
-            </button>
-            <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
-              Message
-            </button>
-          </div>
+          <span className="text-sm text-gray-500">
+            {contact.website}
+          </span>
+          <span className="text-sm text-gray-500">
+            {contact.company}
+          </span>
+          <span className="text-sm text-gray-500">
+            {contact.phone}
+          </span>
         </div>
       </div>
     </>
